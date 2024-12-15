45367c8a64a3dfcc9e5287f93486dacf7217179a
ViacheslavTsybulskiy <126974582+v0ts@users.noreply.github.com>
2024-12-15 15:07:31 +0200

    done

---
+++ app.js
@@ -0,0 +1,94 @@
+const buttonEL = document.querySelector(".game-button");
+const textEl = document.querySelector(".game-status");
+const keyEl = document.getElementById("key");
+const keys = ["c", "5", "9", "a", "j", "a", "g", "v", "h", "o"];
+let currentKeyIndex = 0;
+
+buttonEL.addEventListener("click", (e) => {
+  if (e.target.textContent === "Почати гру") {
+    e.target.textContent = "Закінчити гру";
+    e.target.classList.add("end-game-button");
+    e.target.classList.remove("game-button");
+
+    textEl.textContent = "Статус гри: Гра почалась";
+    keyEl.textContent = keys[currentKeyIndex];
+
+    document.addEventListener("keydown", (event) => {
+      event.preventDefault();
+      if (keyEl.textContent === event.key) {
+        currentKeyIndex++;
+        keyEl.textContent = keys[currentKeyIndex];
+      } else {
+        PNotify.error({
+          text: `Ви натиснули не на ту кнопку!`,
+        });
+      }
+    });
+  } else if (e.target.textContent === "Закінчити гру") {
+    e.target.textContent = "Почати гру";
+    e.target.classList.add("game-button");
+    e.target.classList.remove("end-game-button");
+
+    currentKeyIndex = 0;
+    textEl.textContent = "Статус гри: Гра закінчена";
+    keyEl.textContent = "";
+  }
+});
+
+const chartData = {
+  labels: [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+  ],
+  datasets: [
+    {
+      label: "Продажі за останній місяць",
+      data: [
+        150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550,
+        600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200,
+        1250, 1300, 1350,
+      ],
+      backgroundColor: "#2196f3",
+      borderColor: "#2196f3",
+      borderWidth: 1,
+    },
+  ],
+};
+
+const config = {
+  type: "line",
+  data: chartData,
+  options: {
+    
+  },
+};
+
+const salesChart = new Chart(document.getElementById("sales-chart"), config);
